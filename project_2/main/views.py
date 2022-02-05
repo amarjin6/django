57b22ff444591a120b3b8793f443355712e7ce82
amarjin6 <amarjin6@gmail.com>
2022-02-05 15:25:19 +0300

    third stage

--- project_2/main/views.py
+++ project_2/main/views.py
@@ -1,15 +1,17 @@
 from django.shortcuts import render
 from .models import Input
+from .forms import InputForm
 
 
 # Create your views here.
 def index(request):
     inputs = Input.objects.all()
-
+    form = InputForm
     data = {
         'title': 'Home',
         'panel': 'Control panel',
         'btn_1': 'add',
         'btn_2': 'submit',
-        'input': inputs}
+        'input': inputs,
+        'form': form}
     return render(request, 'main/index.html', data)
